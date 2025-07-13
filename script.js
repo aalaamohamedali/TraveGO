5bf9333e31e700537a42271d190bf2c151cacc87
aalaamohamedali <aalaa.mohamed.alii@gmail.com>
2025-07-13 21:51:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+//icons feather
+feather.replace();
+
+// menu button
+document.getElementById("menuButton").addEventListener("click", () => {
+  document.getElementById("mobileMenu").classList.toggle("hidden");
+});
